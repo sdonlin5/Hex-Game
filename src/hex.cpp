d8e42aafb74018babc37d39eb5c97f1426f6367a
Stephen Donlin <sddonlin@gmail.com>
2025-12-03 15:03:06 -0800

    Added accessors and setters to the Hex class.

--- src/hex.cpp
+++ src/hex.cpp
@@ -14,6 +14,41 @@ Hex::Hex(const int row, const int col) {
 
 Hex::~Hex() = default;
 
+
+// Coordinate accessors
+Coords Hex::GetCoords() const {
+    return coords_;
+}
+
+void Hex::SetCoords(Coords coords) {
+    coords_.col = coords.col;
+    coords_.row = coords.row;
+}
+
+std::string Hex::StringifyCoords() const {
+    // returns the coordinates of hex as a string [Row, Col]
+    const std::string& col = column.at(coords_.col + 1);
+    const std::string row = std::to_string(coords_.row + 1);
+    return "[" + col + row + "]";
+}
+
+
+// State accessors
+state Hex::GetState() const {
+    return state_;
+}
+
+void Hex::SetState(const state state) {
+    state_ = state;
+}
+
+
+// Union-Find helpers (parent/rank)
+void Hex::SetSelfParent() {
+    // sets parent as weak_ptr to self
+    parent_ = weak_from_this();
+}
+
 std::weak_ptr<Hex> Hex::GetParent() const {
     return parent_;
 }
